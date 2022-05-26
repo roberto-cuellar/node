280f1e3e97b88f03db35aa043f1e907312ab189a
Antonio Cuellar <lozano.roberto.2018@gmail.com>
2022-05-25 19:49:27 -0500

    first commit on node repo, add 01 activitym send email using nodemailer

---
+++ 01-sendEmail/sendEmail.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const nodemailer = require('nodemailer');
+
+emailFrom = process.env.USER_FROM;
+emailTo = process.env.USER_TO;
+
+const transporter = nodemailer.createTransport({
+    service: 'hotmail',
+    auth: {
+        user: emailFrom,
+        pass: process.env.USER_KEY
+    }
+})
+
+const mailOptions  = {
+    from: emailFrom,
+    to: emailTo,
+    subject: 'Sending Email using Node.js',
+    text: 'That was easy'
+}
+
+transporter.sendMail(mailOptions,(error,info)=>{
+    error?(console.log(error)):(console.log('Email sent: '+info.response))
+})
