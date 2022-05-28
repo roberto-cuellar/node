801d96c93c7389c60ac0a6dd8e7b8befa973a38b
Antonio Cuellar <lozano.roberto.2018@gmail.com>
2022-05-28 10:00:12 -0500

    add asynchronous behavior

--- 02-webScrapper/searchPlusScraping.js
+++ 02-webScrapper/searchPlusScraping.js
@@ -1,23 +1,41 @@
 
 const cheerio = require("cheerio");
-const { rmSync } = require("fs");
 const puppeteer = require('puppeteer');
+
+
+const urls = [
+    'https://ojs.unipamplona.edu.co/ojsviceinves/index.php/bistua/article/view/205',
+    'https://ojs.unipamplona.edu.co/ojsviceinves/index.php/bistua/article/view/206'
+    // 'https://ojs.unipamplona.edu.co/ojsviceinves/index.php/bistua/article/view/207',
+    // 'https://ojs.unipamplona.edu.co/ojsviceinves/index.php/bistua/article/view/208',
+    // 'https://ojs.unipamplona.edu.co/ojsviceinves/index.php/bistua/article/view/209',
+    // 'https://ojs.unipamplona.edu.co/ojsviceinves/index.php/bistua/article/view/210',
+    // 'https://ojs.unipamplona.edu.co/ojsviceinves/index.php/bistua/article/view/1158',
+    // 'https://ojs.unipamplona.edu.co/ojsviceinves/index.php/bistua/article/view/1125'
+]
 
 let html = [];
 let articulos = [];
+retornar();
 
-(async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('https://ojs.unipamplona.edu.co/ojsviceinves/index.php/bistua/article/view/210');
-    await page.screenshot({
-      path: 'landing-page-1.png',
-      fullPage: true
-    });
-    html = await page.content();
-    scrape(html);  
-    browser.close();
-  })();
+async function busqueda(html,urls) {  
+    for(let i = 0;i<urls.length;i++){                
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto(urls[i]);
+        html = await page.content();
+        scrape(html);  
+        console.log("test 1")
+        browser.close();            
+    }
+}
+
+async function retornar() {
+    const datos = await busqueda(html,urls);
+    console.log("IMPRIMIENDO DESPUES DE RESOLVER TODO LO ANTERIOR")
+    console.log(articulos);
+}
+
 
 class Articulo{
     constructor(
@@ -56,26 +74,26 @@ function scrape(html) {
     
     /// Extracción de los autores
     const titulo = $('h1').text().trim();
-    console.log(titulo);
+    // console.log(titulo);
 
     /// Extracción de los autores
     let autores = []
     $('.authors .name').each(function (idx,el) {
         autores.push($(el).text().trim())
     });
-    console.log('Autores: ',autores);
+    // console.log('Autores: ',autores);
     
     //// Extracción de la afiliación
     let afiliacion = []
     $('.authors .affiliation').each(function (idx,el) {
         afiliacion.push($(el).text().trim())
     })
-    console.log('Afiliación: ',afiliacion);
+    // console.log('Afiliación: ',afiliacion);
 
     
     /// Extracción del doi
     const doi = $('.doi .value').text().trim();
-    console.log('DOI: ',doi);
+    // console.log('DOI: ',doi);
     
     /// Extracción de las palabras clave
     let keywordsVect = []
@@ -83,23 +101,23 @@ function scrape(html) {
     keywords.forEach(element => {
         keywordsVect.push(element.trim())
     })
-    console.log('Palabras clave: ',keywordsVect);
+    // console.log('Palabras clave: ',keywordsVect);
     
     /// Extracción del Resumen
     const resumen = $('.abstract p').text().trim();
-    console.log('Resumen: ',resumen);
+    // console.log('Resumen: ',resumen);
     
     /// Extracción de la cita standard
     const citaStd = $('.csl-right-inline').text().trim();
-    console.log('Cita Estandar: ',citaStd)
+    // console.log('Cita Estandar: ',citaStd)
     
     /// Extracción del link del PDF por medio del atributo href de la etiquta a
     const linkPdf = $('.pdf').attr('href');
-    console.log('Link al PDF: ',linkPdf)
+    // console.log('Link al PDF: ',linkPdf)
     
     /// Extracción del link de la imagen abstract por medio del atributo src de la etiquta img
     const linkImg = $('.cover_image .sub_item img').attr('src');
-    console.log('Link a la imagen: ',linkImg)
+    // console.log('Link a la imagen: ',linkImg)
 
     /// Extracción del volumen al que pertenece 
     const issue = $('.issue .sub_item .value .title').text().trim().split(" ");
@@ -107,11 +125,10 @@ function scrape(html) {
     const num = issue[3];
     //Del año se deben quitar los paréntesis
     const year = issue[4].replace(/["'\(\)]/g, "");
-    console.log('Volumen: ',vol)
-    console.log('Número: ',num)
-    console.log('Año: ',year)
-    console.log("Como clase: -------")
+    // console.log('Volumen: ',vol)
+    // console.log('Número: ',num)
+    // console.log('Año: ',year)
+    //console.log("Como clase: -------")
     articulos.push(new Articulo(titulo,autores,afiliacion,doi,keywordsVect,resumen,citaStd,linkPdf,linkImg,year,vol,num));
     console.log(articulos);
-
 }
